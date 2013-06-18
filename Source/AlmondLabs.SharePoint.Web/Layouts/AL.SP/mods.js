69f15331a746095bf2e30ebfc5c0b745eaf868d6
mrgeom <hramos@almondlabs.com>
2013-06-18 14:22:46 -0400

    sharepoint resources

---
+++ Source/AlmondLabs.SharePoint.Web/Layouts/AL.SP/mods.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    OverrideSearchBox();
+});
+
+function OverrideSearchBox() {
+    jQuery('input.ms-srch-sb-prompt').blur(function () {
+        jQuery("#NavDropdownListContainer").css({ display: "none", visibility: "visible" });
+    });
+    jQuery("input.ms-srch-sb-prompt").click(function (event) {
+        var nodes = jQuery("div.ms-qSuggest-listItem");
+        if (nodes.length > 0) {
+            jQuery("#NavDropdownListContainer").css({ display: "block", visibility: "visible" });
+            return Srch.U.cancelEvent(event.OriginalEvent);
+        }
+        else {
+            jQuery(this).next().click();
+            return Srch.U.cancelEvent(event.OriginalEvent);
+        }
+    });
+}
